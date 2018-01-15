9bdd3edb3da2a26fcd270a6c2500e4c672a8d906
JamieMagee <jamie.magee@gmail.com>
2018-01-15 21:40:04 +0100

    Remove old test

--- __tests__/hashflag-spec.ts
+++
@@ -1,6 +0,0 @@
-import { HashFlag } from '../src/hash-flag';
-
-test('Should greet with message', () => {
-  const greeter = new HashFlag('friend');
-  expect(greeter.greet()).toBe('Bonjour, friend!');
-});
