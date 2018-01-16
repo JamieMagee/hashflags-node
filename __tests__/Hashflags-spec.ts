93f8d064259647f9a01124b277859ab6339bc1a3
JamieMagee <jamie.magee@gmail.com>
2018-01-16 22:52:07 +0100

    Switch to static initialiser class

--- __tests__/Hashflags-spec.ts
+++ __tests__/Hashflags-spec.ts
@@ -1,43 +1,41 @@
 import axios from 'axios';
 import * as MockAdapter from 'axios-mock-adapter';
 import * as fs from 'fs';
-import { Hashflags, IHashflagsJson } from '../src/Hashflags';
+import { URL } from 'url';
+import { Hashflags } from '../src/Hashflags';
 
 /**
  * Tests for the Hashflags class
  */
 
-beforeAll(() => {
-  const mock: MockAdapter = new MockAdapter(axios);
-  const mockData: IHashflagsJson = JSON.parse(
-    fs.readFileSync('./__tests__/activeHashflags.json', 'utf8')
-  );
-  mock
-    .onGet('https://hashflags.blob.core.windows.net/json/activeHashflags')
-    .reply(200, mockData);
-});
-
 describe('Test', () => {
   let hashflags: Hashflags;
 
   beforeAll(async () => {
-    hashflags = new Hashflags();
-    await hashflags.fetch();
+    const mock: MockAdapter = new MockAdapter(axios);
+    const mockData: IHashflagsJson = JSON.parse(
+      fs.readFileSync('./__tests__/activeHashflags.json', 'utf8')
+    );
+    mock
+      .onGet('https://hashflags.blob.core.windows.net/json/activeHashflags')
+      .reply(200, mockData);
+    hashflags = await Hashflags.CREATE();
   });
-
-  it('Should have baseUrl available', () => {
-    expect(hashflags.baseUrl).not.toBeUndefined();
-    expect(hashflags.baseUrl.host).not.toBeUndefined();
-  });
-
-  it('Should have activeHashflags available', () => {
+  it('Should have activeHashflags map available', () => {
     expect(hashflags.activeHashflags).not.toBeUndefined();
   });
 
   it('Should contain olympictorchrelay hashtag', () => {
     expect(hashflags.activeHashflags.has('olympictorchrelay')).toBeTruthy();
     expect(hashflags.activeHashflags.get('olympictorchrelay')).toEqual(
-      'OlympicFlameEmoji/OlympicFlameEmoji.png'
+      new URL(
+        'https://abs.twimg.com/hashflags/OlympicFlameEmoji/OlympicFlameEmoji.png'
+      )
     );
   });
 });
+
+interface IHashflagsJson {
+  hashflagBaseUrl: string;
+  activeHashflags: { [hashflag: string]: string };
+}
--- src/Hashflags.ts
+++ src/Hashflags.ts
@@ -6,33 +6,48 @@ import { URL } from 'url';
  */
 export class Hashflags {
   // tslint:disable-next-line:variable-name
-  private _baseUrl: URL;
-  // tslint:disable-next-line:variable-name
-  private _activeHashflags: Map<string, string>;
+  private _activeHashflags: Map<string, URL>;
 
-  public async fetch(): Promise<void> {
-    // tslint:disable-next-line:no-backbone-get-set-outside-model
+  private constructor() {}
+
+  /**
+   * A static constructor for the Hashflags class.
+   * This is necessary as we need to make an API call to populate the list of active hashflags.
+   * @returns A Hashflags instance.
+   */
+  public static async CREATE(): Promise<Hashflags> {
+    const clazz: Hashflags = new Hashflags();
+    await clazz.initialize();
+
+    return clazz;
+  }
+
+  public async initialize(): Promise<void> {
     await axios
       .get('https://hashflags.blob.core.windows.net/json/activeHashflags')
       .then((response: AxiosResponse<IHashflagsJson>) => {
-        this._baseUrl = new URL(response.data.hashflagBaseUrl);
         this._activeHashflags = new Map();
         Object.keys(response.data.activeHashflags).forEach((key: string) => {
-          this._activeHashflags.set(key, response.data.activeHashflags[key]);
+          this._activeHashflags.set(
+            key,
+            new URL(
+              response.data.hashflagBaseUrl + response.data.activeHashflags[key]
+            )
+          );
         });
       });
   }
 
-  public get baseUrl(): URL {
-    return this._baseUrl;
-  }
-
-  public get activeHashflags(): Map<string, string> {
+  /**
+   * Get a map of all the current active hashflags, and full URL to their image.
+   * @returns Map from hashtag to hashflag image
+   */
+  public get activeHashflags(): Map<string, URL> {
     return this._activeHashflags;
   }
 }
 
-export interface IHashflagsJson {
+interface IHashflagsJson {
   hashflagBaseUrl: string;
   activeHashflags: { [hashflag: string]: string };
 }
